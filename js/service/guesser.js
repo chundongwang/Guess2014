87ffbd12d43646dc846d7903bdd3f0a0e92e1b8a
hegara <hegara_lee@hotmail.com>
2014-06-03 22:59:45 -0700

    Make group clickable and show modal for betting

--- js/service/guesser.js
+++ js/service/guesser.js
@@ -8,9 +8,31 @@ WorldCupApp.getModule().factory("Guesser", ['$http', function($http) {
   function listA(groupName, successCallback, errorCallback) {
     $http({url: WorldCupApp.getRoot() + '/list/'+groupName, method: 'GET'}).success(successCallback).error(errorCallback);
   }
+  
+  function betAll(successCallback, errorCallback) {
+    //TODO: replace with real backend call
+    successCallback([
+                     {
+                         "guess_a": "1",
+                         "guess_b": "3",
+                         "matchid": 18
+                     },
+                     {
+                         "guess_a": "1",
+                         "guess_b": "1",
+                         "matchid": 4
+                     },
+                     {
+                         "guess_a": "0",
+                         "guess_b": "2",
+                         "matchid": 34
+                     }
+                 ]);
+  }
 
   return {
     listAll: listAll,
-    listA: listA
+    listA: listA,
+    betAll: betAll
   };
 }]);
--- js/view/group.js
+++ js/view/group.js
@@ -1,9 +1,8 @@
 'use strict';
 
-WorldCupApp.getModule().controller('GroupCtrl', ['$scope', '$location', 'Guesser', function($scope, $location, Guesser) {
-  
+WorldCupApp.getModule().controller('GroupCtrl', ['$scope', '$location', '$q', 'Guesser', 
+                                                 function($scope, $location, $q, Guesser) {
 
-  
   var stage = $location.search().stage;
   stage = stage || 'Group A';
   $scope.stage = stage;
@@ -12,33 +11,61 @@ WorldCupApp.getModule().controller('GroupCtrl', ['$scope', '$location', 'Guesser
     nickName:WorldCupApp.user_nickname, 
     loginUrl:WorldCupApp.login_url
   };
-  $scope.bettable = !!$scope.loginInfo.nickName;
+  $scope.loggedIn = !!$scope.loginInfo.nickName;
 
   $scope.bet = function(mid) {
-    // If bettable, display edit box for that match
-    if ($scope.bettable) {
+    // If loggedIn, display edit box for that match
+    if ($scope.loggedIn) {
       $scope.matches.forEach(function(e){if(e.matchid == mid){e.edit = true;}});
     }
-    // Otherwise, redirect to bet
+    // Otherwise, redirect to signin
     else {
       window.location.href = $scope.loginInfo.loginUrl;
     }
   }
-  
+
   $scope.save = function(mid, score_a, score_b) {
     console.log(mid + ' ' + score_a + ' ' + score_b);
     $scope.matches.forEach(function(e){if(e.matchid == mid){e.edit = false;}});
   }
   
-  function updateAll() {
-    Guesser.listA(stage, function(data){
-      
-      var matches = data[0];
-      matches.forEach(function(e){e.edit = false;});
-      $scope.matches = matches;
-    });
+  $scope.highlight = function(panel){
+    console.log(panel);
   }
   
   updateAll();
+  
+  function asyncListA() {
+    var deferred = $q.defer();
+    Guesser.listA(stage, function(data){
+      var matches = data[0];
+      matches.forEach(function(e){e.edit = false;});
+      deferred.resolve(matches);
+    });
+    return deferred.promise;
+  }
+  
+  function asyncBetAll() {
+    var deferred = $q.defer();
+    Guesser.betAll(function(data){
+      var guesses = [];
+      data.forEach(function(e){guesses[e.matchid]=e;});
+      deferred.resolve(data);
+    });
+    return deferred.promise;
+  }
+
+  function updateAll() {
+    asyncListA().then(function(matches){
+      asyncBetAll().then(function(guesses){
+        // After get all the data, massage the guess data to initialize the object for unguessed matches
+        matches.forEach(function(m){if (!guesses[m.matchid]) {guesses[m.matchid] = {matchid:m.matchid};}});
+        // Then set them to the scope
+        $scope.matches = matches;
+        $scope.guesses = guesses;
+      });
+    });
+  }
+
 
 }]);
