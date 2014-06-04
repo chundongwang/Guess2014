383d8424602ff4fc8d97b05c87606e3a5b479125
hegara <hegara_lee@hotmail.com>
2014-06-03 19:54:27 -0700

    Add listA for list a single group and use that to present group view

--- js/view/group.js
+++ js/view/group.js
@@ -1,6 +1,18 @@
 'use strict';
 
-WorldCupApp.getModule().controller('GroupCtrl', ['$scope', '$location', function($scope, $location) {
-  $scope.stage = $location.search().stage;
+WorldCupApp.getModule().controller('GroupCtrl', ['$scope', '$location', 'Guesser', function($scope, $location, Guesser) {
+  
+  var stage = $location.search().stage;
+  stage = stage || 'Group A';
+  
+  $scope.stage = stage;
+
+  function updateAll() {
+    Guesser.listA(stage, function(data){
+      $scope.matches = data[0];
+    });
+  }
+  
+  updateAll();
 
 }]);
