f9eafa81fa2c4ae2b0fb34d08a7418123b7f73b2
hegara <hegara_lee@hotmail.com>
2014-06-03 20:14:46 -0700

    Display 'bet' button

--- js/view/group.js
+++ js/view/group.js
@@ -4,9 +4,13 @@ WorldCupApp.getModule().controller('GroupCtrl', ['$scope', '$location', 'Guesser
   
   var stage = $location.search().stage;
   stage = stage || 'Group A';
-  
   $scope.stage = stage;
-
+  
+  $scope.loginInfo = {
+    nickName:WorldCupApp.user_nickname, 
+    loginUrl:WorldCupApp.login_url
+  };
+  $scope.bettable = !!$scope.loginInfo.nickName;
   function updateAll() {
     Guesser.listA(stage, function(data){
       $scope.matches = data[0];
